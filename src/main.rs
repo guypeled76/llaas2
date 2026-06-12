3f6287fae603ef6e12660efc651dbe8fcc596dfb
gremlin <31251087+guypeled76@users.noreply.github.com>
2026-06-12 20:08:45 +0200

    add part of speach

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use tch::{Device};
 
 fn main() {
     translate();
+    pos();
 }
 
 
@@ -37,6 +38,25 @@ fn translate() {
         Err(e) => {
             eprintln!("Error during translation: {}", e);
             return;
+        }
+    }
+}
+
+
+fn pos() {
+    let model = rust_bert::pipelines::pos_tagging::POSModel::new(Default::default()).unwrap();
+    let input = [
+        "Are you sure this is working?", 
+        "本当にこれでうまくいってますか？",
+        "Er du sikker på, at dette virker?",
+        "¿Estás seguro de que esto funciona?"
+    ];
+    let output = model.predict(&input);
+    
+    for(i, sentence) in output.iter().enumerate() {
+        println!("Sentence {}:", i + 1);
+        for (word, tag) in sentence.iter().enumerate() {
+            println!("{}:{} - {} ({})", word + 1, tag.word, tag.label, tag.score);
         }
     }
 }
