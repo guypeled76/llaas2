87ca013c3c0dbfec2848feb4156f5dc939119659
gremlin <31251087+guypeled76@users.noreply.github.com>
2026-06-12 19:36:41 +0200

    translation

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use rust_bert::pipelines::{common::ModelType, translation::{
+    Language,  
+    TranslationModelBuilder,
+}};
+
+use tch::{Device};
+
+fn main() {
+    translate();
+}
+
+
+fn translate() {
+    let model = TranslationModelBuilder::new()
+        .with_device(Device::cuda_if_available())
+        .with_model_type(ModelType::Marian)
+        .with_source_languages(vec![Language::English])
+        .with_target_languages(vec![Language::Spanish])
+        .create_model()
+        .unwrap();
+
+    let input = [
+        "Hello, how are you?", 
+        "This is a test of the translation pipeline."
+    ];
+    
+    let output = model.translate(
+        &input, 
+        None, 
+        Language::Spanish
+    );
+
+    println!("{:?}", output);
+}
