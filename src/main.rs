194d747669f699630a07492df95add8a47666b55
gremlin <31251087+guypeled76@users.noreply.github.com>
2026-06-12 20:15:48 +0200

    keyword extraction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_bert::pipelines::{common::ModelType, translation::{
+use rust_bert::pipelines::{common::ModelType, keywords_extraction::KeywordExtractionModel, translation::{
     Language,  
     TranslationModelBuilder,
 }};
@@ -8,6 +8,8 @@ use tch::{Device};
 fn main() {
     translate();
     pos();
+    sentiment();
+    keyword_extraction();
 }
 
 
@@ -58,5 +60,31 @@ fn pos() {
         for (word, tag) in sentence.iter().enumerate() {
             println!("{}:{} - {} ({})", word + 1, tag.word, tag.label, tag.score);
         }
+    }
+}
+
+fn sentiment() {
+    let model = rust_bert::pipelines::sentiment::SentimentModel::new(Default::default()).unwrap();
+    let input = [
+        "I love this product!", 
+        "This is the worst experience I've ever had."
+    ];
+    let output = model.predict(&input);
+    
+    for(i, sentiment) in output.iter().enumerate() {
+        println!("Sentence {}: {:#?} (score: {})", i + 1, sentiment.polarity, sentiment.score);
+    }
+}
+
+fn keyword_extraction() {
+    let model = KeywordExtractionModel::new(Default::default()).unwrap();
+    let input = [
+        "Rust is a systems programming language focused on safety and performance.", 
+        "The quick brown fox jumps over the lazy dog."
+    ];
+    let output = model.predict(&input);
+    
+    for(i, keywords) in output.iter().enumerate() {
+        println!("Sentence {}: Keywords: {:?}", i + 1, keywords);
     }
 }
